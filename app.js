8278a18ce41a1f62a94ed9ecb645e022f917b1de
Amjath73 <amjathsanker4@gmail.com>
2024-06-12 21:37:29 +0530

    add military

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { militarymodel } = require("./models/military")
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://amjath:itsArkingtime7@cluster0.n01k0zd.mongodb.net/militaryDB?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add", (req, res) => {
+    let input = req.body
+    let military = new militarymodel(input)
+    console.log(military)
+    military.save()
+    res.json({ "status": "test" })
+})
+
+app.listen(8081, ()=>{
+    console.log("server started")
+
+})
